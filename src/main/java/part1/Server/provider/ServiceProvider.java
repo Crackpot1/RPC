f8384f1df5dee1464e32a439d34266fb80643650
Crackpot1 <18846794509@163.com>
2025-05-21 13:48:06 +0800

    令牌桶算法限流

--- src/main/java/part1/Server/provider/ServiceProvider.java
+++ src/main/java/part1/Server/provider/ServiceProvider.java
@@ -1,5 +1,10 @@
 package part1.Server.provider;
 
+import part1.Server.ratelimit.provider.RateLimitProvider;
+import part1.Server.serviceRegister.impl.ZKServiceRegister;
+import part1.Server.serviceRegister.ServiceRegister;
+
+import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,27 +13,41 @@ import java.util.Map;
  * @version 1.0
  * @create 2024/2/16 17:35
  */
-//本地服务存放器
 public class ServiceProvider {
-    //集合中存放服务的实例
     private Map<String,Object> interfaceProvider;
 
-    public ServiceProvider(){
+    private int port;
+    private String host;
+    //注册服务类
+    private ServiceRegister serviceRegister;
+    //限流器
+    private RateLimitProvider rateLimitProvider;
+    public ServiceProvider(String host,int port){
+        //需要传入服务端自身的网络地址
+        this.host=host;
+        this.port=port;
         this.interfaceProvider=new HashMap<>();
+        this.serviceRegister=new ZKServiceRegister();
+        this.rateLimitProvider=new RateLimitProvider();
     }
-    //本地注册服务
 
-    public void provideServiceInterface(Object service){
+    public void provideServiceInterface(Object service,boolean canRetry){
         String serviceName=service.getClass().getName();
         Class<?>[] interfaceName=service.getClass().getInterfaces();
 
         for (Class<?> clazz:interfaceName){
+            //本机的映射表
             interfaceProvider.put(clazz.getName(),service);
+            //在注册中心注册服务
+            serviceRegister.register(clazz.getName(),new InetSocketAddress(host,port),canRetry);
         }
-
     }
-    //获取服务实例
+
     public Object getService(String interfaceName){
         return interfaceProvider.get(interfaceName);
+    }
+
+    public RateLimitProvider getRateLimitProvider(){
+        return rateLimitProvider;
     }
 }
